81319718e46b3090989a603ca810d7f92874b510
zhi176 <52850311+zhi176@users.noreply.github.com>
2022-09-19 10:38:46 +0800

    Update GitDemo.java

--- src/main/java/edu/xdu/zhi/GitDemo.java
+++ src/main/java/edu/xdu/zhi/GitDemo.java
@@ -8,5 +8,6 @@ public class GitDemo {
         System.out.println("master test");
         System.out.println("hot_fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
