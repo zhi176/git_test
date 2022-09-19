31fc7afda6cd3c3328e69433f390b4635f6ca793
zhiwentao <zwt13962936376ll@163.com>
2022-09-19 09:45:01 +0800

    hotfixcommit1

--- src/main/java/edu/xdu/zhi/GitDemo.java
+++ src/main/java/edu/xdu/zhi/GitDemo.java
@@ -4,5 +4,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello world");
+        System.out.println("hello git4");
     }
 }
