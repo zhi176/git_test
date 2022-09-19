6ff50cdd59837142733ed0b8ddf7fbf139047dd9
zhiwentao <zwt13962936376ll@163.com>
2022-09-19 09:34:42 +0800

    secondcommit

--- src/main/java/edu/xdu/zhi/GitDemo.java
+++ src/main/java/edu/xdu/zhi/GitDemo.java
@@ -2,6 +2,7 @@ package edu.xdu.zhi;
 
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.printf("hello git");
+        System.out.println("hello git");
+        System.out.println("hello world");
     }
 }
